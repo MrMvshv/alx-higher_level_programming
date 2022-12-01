7570bab0be3e7a0ccaec55bc79a54e85e0acf573
MrMvshv <machariajosphat001@gmail.com>
2022-12-01 14:37:08 +0000

    1 too?

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
 import sys
-a = len(sys.argv)
-if a == 1:
-    print("0 arguments.")
-elif a == 2:
-    print("1 argument:\n1: {}".format(sys.argv[1]))
-else:
-    print("{} arguments:".format(a-1))
-    for x in range(1, a):
-        print("{}: {}".format(x, sys.argv[x]))
+
+
+def magombano():
+    a = len(sys.argv)
+    if a == 1:
+        print("0 arguments.")
+    elif a == 2:
+        print("1 argument:\n1: {}".format(sys.argv[1]))
+    else:
+        print("{} arguments:".format(a-1))
+        for x in range(1, a):
+            print("{}: {}".format(x, sys.argv[x]))
+
+
+
+if __name__ == '__main__':
+    magombano()
