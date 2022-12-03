24795c2daae82e46c7705a3422ba4ee89dfbdd1c
MrMvshv <machariajosphat001@gmail.com>
2022-12-03 17:44:12 +0000

    3

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def print_reversed_list_integer(my_list=[]):
+    x = len(my_list) - 1
+    while x >= 0:
+        print("{:d}".format(my_list[x]))
+        x = x - 1
+
+
+if __name__ == '__main__':
+    print_list_integer([])
