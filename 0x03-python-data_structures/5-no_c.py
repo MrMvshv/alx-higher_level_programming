83c0c57e6e2d398459e734e1db78708ca922cd11
MrMvshv <machariajosphat001@gmail.com>
2022-12-03 18:33:06 +0000

    55

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+def no_c(my_string):
+   if my_string is not None:
+        new = []
+        for c in my_string:
+            if c == 'C' or c == 'c':
+                continue
+            new.append(c)
+        newS = ''.join(map(str, new))
+        return newS
+
+
+if __name__ == '__main__':
+    print_list_integer([])
