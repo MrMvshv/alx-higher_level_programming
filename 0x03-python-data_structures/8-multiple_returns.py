1d84f3603753d2d9df1d89cfb246d58dbb27910f
MrMvshv <machariajosphat001@gmail.com>
2022-12-04 05:27:38 +0000

    8

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def multiple_returns(sentence):
+    if len(sentence) == 0:
+        d = 0
+        c = None
+    else:
+        d = len(sentence)
+        c = sentence[0]
+    tup_s = (d, c)
+    return tup_s
+
+if __name__ == '__main__':
+    multiple_returns()
