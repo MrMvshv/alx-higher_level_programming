1f6b9c7cc9ceb357e0134889a4487c45f3c8a831
MrMvshv <machariajosphat001@gmail.com>
2022-12-09 18:14:47 +0000

    tr

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
+    new = [[]]
     if matrix is None or matrix == [[]]:
-        return None
+        return new
     new = []
     for y in matrix:
         new.append(list(map(lambda x: (x ** 2), y)))
