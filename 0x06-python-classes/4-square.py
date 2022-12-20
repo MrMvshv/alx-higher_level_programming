641fe7bb31c5ccf09fca5f1830cea218a1e468e7
MrMvshv <machariajosphat001@gmail.com>
2022-12-20 18:04:59 +0000

    4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+
+
+""" class square definition
+    optional size, error checking
+    size
+"""
+
+
+class Square:
+    """ square class
+        Attributes:
+           size(int): size of sq
+    """
+    def __init__(self, size=0):
+        """ initialize square
+            Args:
+                size: of square
+            Raises:
+                TypeError: not int
+                ValueError: < 0
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """ area gets area
+            returns: area
+        """
+        return (self.__size ** 2)
+    @property
+    def size(self):
+        """ size getter
+	    returns size
+	"""
+        return self.__size
+    @size.setter
+    def size(self, value):
+        """ size setter
+            set value with error catching
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
