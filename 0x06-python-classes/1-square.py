ecd1b1a6552e2471f42a90b92c9019f9fe1e078c
MrMvshv <machariajosphat001@gmail.com>
2022-12-20 09:17:27 +0000

    1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+
+""" class square definition
+
+    size
+"""
+
+
+class Square:
+    """ square class
+        Attributes:
+           size(any): size of sq
+    """
+    def __init__(self, size):
+        """ initialize square
+            to size
+        """
+        self.__size = size
