a1a51a878f1a23cef91e5131e21c19c295f1e19e
MrMvshv <machariajosphat001@gmail.com>
2023-01-09 19:28:33 +0000

    0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -2,9 +2,11 @@
 """ lookup function
     looks up a class
 """
+
+
 def lookup(obj):
-    """ returns the list of available 
-        attributes and methods 
+    """ returns the list of available
+        attributes and methods
         of an object
     """
     return (dir(obj))
