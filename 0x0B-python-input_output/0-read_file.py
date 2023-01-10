839afb22048ef10eaaac6d0c081d337f22d16910
MrMvshv <machariajosphat001@gmail.com>
2023-01-10 16:20:15 +0000

    0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,10 +2,12 @@
 """ read_file reads a file
     then prints it to stdout
 """
+
+
 def read_file(filename=""):
     """ reads a file
         then prints it efficiently
     """
     with open(filename, encoding="utf-8") as f:
         for line in f:
-            print(line, end='') 
+            print(line, end='')
