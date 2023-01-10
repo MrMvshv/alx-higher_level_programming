3158db85d963e6484701f31dd8f51cfe6d60a155
MrMvshv <machariajosphat001@gmail.com>
2023-01-10 14:13:13 +0000

    5

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" empty class
+    called base geometry
+"""
+
+
+class BaseGeometry:
+    """ is an empty class
+        definition
+        contains nothing
+    """
+    pass
