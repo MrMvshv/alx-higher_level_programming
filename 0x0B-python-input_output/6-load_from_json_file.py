9102b0919d0f0800ab347c2c2f25e15f8e6630ff
MrMvshv <machariajosphat001@gmail.com>
2023-01-10 18:25:41 +0000

    66

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -12,4 +12,4 @@ def load_from_json_file(filename):
         returns json of file
     """
     with open(filename, 'r', encoding="utf-8") as f:
-        return json.dumps(f.read())
+        return json.loads(f.read())
