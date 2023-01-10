40fc78af6b911f9ace85542b6aab4746dc9979d3
MrMvshv <machariajosphat001@gmail.com>
2023-01-10 17:10:56 +0000

    6

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" creates an object from a file
+    using json representation
+"""
+import json
+
+
+def load_from_json_file(filename):
+    """ loads object
+        from json file
+        no exceptions
+        returns json of file
+    """
+    with open(filename, 'r', encoding="utf-8") as f:
+        return json.dumps(f.read())
