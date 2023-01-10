ebe651640c361612e4ed843e5b7f95458ee801db
MrMvshv <machariajosphat001@gmail.com>
2023-01-10 14:06:20 +0000

    3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" is same class function
+    returns true or false
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """ returns True if is an instance
+        otherwise false
+    """
+    return isinstance(obj, a_class)
