ad91c88adedefdccb4d6981d1cf84da7e30cdd0c
MrMvshv <machariajosphat001@gmail.com>
2023-01-10 15:00:47 +0000

    10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" class definition
+    inherits from base geometry
+    with private width and height
+    using integer validator
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ is a  class definition
+        contains initializer
+    """
+    def __init__(self, size):
+        """ init function
+            private variable size
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
