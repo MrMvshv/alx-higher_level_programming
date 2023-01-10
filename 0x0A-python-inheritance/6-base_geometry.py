588808d8a6c37c27b24daf5da512e09b1e96c093
MrMvshv <machariajosphat001@gmail.com>
2023-01-10 14:17:41 +0000

    6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" class definition
+    called base geometry
+    with dummy function
+"""
+
+
+class BaseGeometry:
+    """ is a  class definition
+        contains area function
+    """
+    def area(self):
+        """ area function
+            raises exception
+        """
+        raise Exception("area() is not implemented")
