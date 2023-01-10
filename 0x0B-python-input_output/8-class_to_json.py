cf46086ec2f951540c51e248365a548065522d3a
MrMvshv <machariajosphat001@gmail.com>
2023-01-10 19:58:18 +0000

    88

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -8,7 +8,7 @@
 
 def class_to_json(obj):
     """obj is an instance of a Class
-        All attributes of the obj Class are serializable: 
+        All attributes of the obj Class are serializable:
         list, dictionary, string, integer and boolean
     """
     return obj.__dict__
