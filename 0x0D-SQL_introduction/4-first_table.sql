453e66a2e1b666d7cf326a7bdacd990fdc88fe45
MrMvshv <machariajosphat001@gmail.com>
2023-02-16 12:21:14 +0300

    4

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,3 +1,5 @@
 -- creates tables in db
 -- db provided as argument
-CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS id_not_null (
+    id INT NOT NULL,
+    name VARCHAR(256));
