138847c35ba9e413eafa1e7197ce62c8a6b06884
MrMvshv <machariajosphat001@gmail.com>
2023-02-16 12:35:29 +0300

    4

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,6 +1,6 @@
 -- creates tables in db
 -- db provided as argument
-CREATE TABLE IF NOT EXISTS id_not_null (
-    id INT NOT NULL DEFAULT 1,
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
     name VARCHAR(256)
 );
--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -2,4 +2,5 @@
 -- should not fail
 CREATE TABLE IF NOT EXISTS force_name (
     id INT NOT NULL DEFAULT 1,
-    name VARCHAR(256) NOT NULL);
+    name VARCHAR(256)
+);
