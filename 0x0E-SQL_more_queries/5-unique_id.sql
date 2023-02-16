39358fa6a9bb8d6fb8059dbbf70193382478d2a9
MrMvshv <machariajosphat001@gmail.com>
2023-02-16 12:41:32 +0300

    5

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,5 +1,6 @@
 -- create table on server
 -- should not fail
-CREATE TABLE IF NOT EXISTS force_name (
-    id INT NOT NULL DEFAULT 1,
-    name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS unique_id (
+    id INT DEFAULT 1 UNIQUE,
+    name VARCHAR(256)
+);
