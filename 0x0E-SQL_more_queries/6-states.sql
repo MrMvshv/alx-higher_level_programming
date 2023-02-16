11f1796768b8603f05fdb73fbdd7213c7d29bdd4
MrMvshv <machariajosphat001@gmail.com>
2023-02-16 12:48:27 +0300

    6

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,7 +1,7 @@
 -- create db & table on server
 -- should not fail
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (
     PRIMARY KEY(`id`),
     id INT NOT NULL AUTO_INCREMENT,
