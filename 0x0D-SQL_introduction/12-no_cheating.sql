ac0ea4b7a69b66b09f9ea5b84b1fd7827c05f328
MrMvshv <machariajosphat001@gmail.com>
2023-02-16 10:51:28 +0300

    13

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,5 @@
+-- updates score of Bob to 10
+-- second_table from the database provided
+UPDATE second_table
+SET score = 10
+WHERE name = 'Bob';
---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,3 @@
+-- updates score of Bob to 10
+-- second_table from the database provided
+DELETE FROM second_table WHERE score <= 5;
