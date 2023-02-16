c9480c54d4f80fe51815651478bc7c8b2aebade5
MrMvshv <machariajosphat001@gmail.com>
2023-02-16 10:43:45 +0300

    11

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,3 +1,3 @@
 -- all rows of table in db sorted descending
 -- second_table from the database hbtn_0c_0
-SELECT score, name FROM second_table ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
