443c1e06742d52d042788ad08e353eec038da81e
MrMvshv <machariajosphat001@gmail.com>
2023-02-17 04:08:56 +0300

    10

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,8 @@
--- create table on server
--- should not fail
-CREATE TABLE IF NOT EXISTS force_name (
-    id INT NOT NULL DEFAULT 1,
-    name VARCHAR(256) NOT NULL);
+-- list shows that have at least 1 genre
+-- use only one select
+SELECT s.`title`, sg.`genre_id`
+FROM tv_shows AS s
+JOIN tv_show_genres AS sg ON s.`id` = sg.`show_id`
+JOIN tv_genres AS g ON sg.`genre_id` = g.`id`
+GROUP BY s.`title`, sg.`genre_id`
+HAVING COUNT(*) >= 1 ORDER BY s.`title`, sg.`genre_id`;
