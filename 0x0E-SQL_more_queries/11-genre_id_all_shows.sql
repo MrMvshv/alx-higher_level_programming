491dfc0ae26d221f6587caeacf73ba1c4d3081a1
MrMvshv <machariajosphat001@gmail.com>
2023-02-17 04:15:55 +0300

    11

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,8 @@
--- create table on server
--- should not fail
-CREATE TABLE IF NOT EXISTS force_name (
-    id INT NOT NULL DEFAULT 1,
-    name VARCHAR(256) NOT NULL);
+-- list all shows in db
+-- use only one select
+SELECT s.`title`, COALESCE(sg.`genre_id`, 'NULL') AS genre_id
+FROM tv_shows AS s
+LEFT JOIN tv_show_genres AS sg ON s.`id` = sg.`show_id`
+LEFT JOIN tv_genres AS g ON sg.`genre_id` = g.`id`
+GROUP BY s.`title`, sg.`genre_id`
+ORDER BY s.`title`, sg.`genre_id`;
