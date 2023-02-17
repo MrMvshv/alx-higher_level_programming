b9455efd31d689729381841e218544964152a523
MrMvshv <machariajosphat001@gmail.com>
2023-02-17 05:00:35 +0300

    14

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,5 +1,8 @@
--- create table on server
--- should not fail
-CREATE TABLE IF NOT EXISTS force_name (
-    id INT NOT NULL DEFAULT 1,
-    name VARCHAR(256) NOT NULL);
+-- list all genres and number of linked shows
+-- in db use one select
+SELECT g.`name` 
+FROM tv_genres AS g
+INNER JOIN tv_show_genres AS sg ON g.`id` = sg.`genre_id`
+INNER JOIN tv_shows AS s ON sg.`show_id` = s.`id`
+WHERE s.`title` = 'Dexter'
+ORDER BY g.`name`;
