7d8f63c18d419b17ff63b38e6bd3c997c77be1bf
MrMvshv <machariajosphat001@gmail.com>
2023-02-17 05:08:27 +0300

    15

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,5 +1,8 @@
--- create table on server
--- should not fail
-CREATE TABLE IF NOT EXISTS force_name (
-    id INT NOT NULL DEFAULT 1,
-    name VARCHAR(256) NOT NULL);
+-- list all comedy shows
+-- in db use one select
+SELECT s.`title` 
+FROM tv_shows AS s
+INNER JOIN tv_show_genres AS sg ON s.`id` = sg.`show_id`
+INNER JOIN tv_genres AS g ON sg.`genre_id` = g.`id`
+WHERE g.`name` = 'Comedy'
+ORDER BY s.`title`;
