07df957e57c3748d4dfa73477e1e8a7b03062ec2
MrMvshv <machariajosphat001@gmail.com>
2023-02-17 04:35:42 +0300

    13

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,8 @@
--- create table on server
--- should not fail
-CREATE TABLE IF NOT EXISTS force_name (
-    id INT NOT NULL DEFAULT 1,
-    name VARCHAR(256) NOT NULL);
+-- list all genres and number of linked shows
+-- in db use one select
+SELECT g.`name` AS genre, COUNT(sg.`show_id`) AS number_of_shows
+FROM tv_genres AS g
+INNER JOIN tv_show_genres AS sg ON g.`id` = sg.`genre_id`
+GROUP BY genre
+HAVING COUNT(sg.`show_id`) > 0
+ORDER BY number_of_shows DESC;
