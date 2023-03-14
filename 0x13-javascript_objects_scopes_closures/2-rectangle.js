50cf6b3dcee6406a55335ff321bf6d3c45ea0b52
MrMvshv <machariajosphat001@gmail.com>
2023-03-14 17:48:57 +0000

    gushed

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,4 +1,10 @@
 #!/usr/bin/node
 
-class Rectangle {
-}
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (h > 0 && w > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+};
