6b61ebb5bfd729d1e8e86d8b05ff71698fb2cbb9
MrMvshv <machariajosphat001@gmail.com>
2023-03-14 18:09:54 +0000

    gushed

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const Sq = require('./5-square');
+module.exports = class Square extends Sq {
+  charPrint (c) {
+    if (c) {
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
+      }
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        console.log('X'.repeat(this.width));
+      }
+    }
+  }
+};
