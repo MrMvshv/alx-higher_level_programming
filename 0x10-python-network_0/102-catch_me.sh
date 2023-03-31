3dd34fb628decceda03e4c3eeeadaf038fdf9679
MrMvshv <machariajosphat001@gmail.com>
2023-03-31 12:14:42 +0000

    102 catch me if you can, ip 0.0.0.0?

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # makes request causing server to respond You got me!
-curl -sL -X PUT -H "Origin: School" -d "user_id=98" 84.72.72.228:5000/catch_me
+curl -sL -X PUT -H "Origin: School" -d "user_id=98" 0.0.0.0:5000/catch_me 
