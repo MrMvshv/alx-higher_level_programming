9eb1a4d04c29d0b13fe93a08b505e14bc931e8bf
MrMvshv <machariajosphat001@gmail.com>
2023-03-31 14:02:20 +0000

    urllib task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+ fetches https://alx-intranet.hbtn.io/status using urllib
+"""
+import urllib.request
+
+with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    data = response.read()
+print("Body response:")
+print("\t- type: ", end="")
+print(type(data))
+print("\t- content:", end="")
+print(data)
+print("\t- utf8 content:", end="")
+print(data.decode("utf-8"))
