5093477b452406e5a972ec3ac2ae4e38649c721e
MrMvshv <machariajosphat001@gmail.com>
2023-04-01 16:09:41 +0000

    Interview challenge

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python3
 """
-post an email to a url, display response using requests
+connect to github and display id using basic auth params
 """
 if __name__ == "__main__":
     import requests
     import sys
 
-    url = sys.argv[1]
-    email = {'email': sys.argv[2]}
+    repname = sys.argv[1]
+    owname = sys.argv[2]
 
-    response = requests.post(url, data=email)
-    body = response.text
-    print(body)
+    url = f'https://api.github.com/repos/{owname}/{repname}/commits'
+
+    response = requests.get(url, params={'per_page': 10})
+
+    if response.status_code == 200:
+        data = response.json()
+        for comm in data:
+            print(f"{comm['sha']}: {comm['commit']['author']['name']}")
+    else:
+        print("Error")
