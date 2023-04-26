cb4a6828205d8bba91d20a6ecd8297e53d577762
MrMvshv <machariajosphat001@gmail.com>
2023-04-26 18:09:43 +0000

    JavaScript scraping advanced

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -2,7 +2,7 @@
 /**
  * This script prints all characters
  * of a star wars movie
- * uses  the Star wars API
+ * uses the Star Wars API
  * first argument is movie id
  * Display one character name by line
  */
@@ -10,22 +10,28 @@ const request = require('request');
 const movie = process.argv[2];
 const url = 'https://swapi-api.alx-tools.com/api/films/' + movie;
 
+function printCharacters (characters, index) {
+  if (index >= characters.length) {
+    return;
+  }
+
+  request.get(characters[index], (error, response, body) => {
+    if (error) {
+      console.error('Error:', error);
+    } else {
+      const data = JSON.parse(body);
+      console.log(data.name);
+      printCharacters(characters, index + 1);
+    }
+  });
+}
+
 request.get(url, (error, response, body) => {
   if (error) {
     console.error('Error:', error);
   } else {
     const result = JSON.parse(body);
-    const list = result.characters;
-
-    list.forEach((actor) => {
-      request.get(actor, (error, response, body) => {
-        if (error) {
-          console.error('Error:', error);
-        } else {
-          const data = JSON.parse(body);
-          console.log(data.name);
-        }
-      });
-    });
+    const characters = result.characters;
+    printCharacters(characters, 0);
   }
 });
