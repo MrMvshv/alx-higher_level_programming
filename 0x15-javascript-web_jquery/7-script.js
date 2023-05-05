1beb3ba6d177197e92420daea91d76d96073ad0b
MrMvshv <machariajosphat001@gmail.com>
2023-05-05 14:57:37 +0000

    script 7

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -2,17 +2,13 @@
  *  fetches the character name
  *  from the url and renders in html
  */
-const request = require('request');
+'use strict';
+
 const url = 'https://swapi-api.alx-tools.com/api/people/5/?format=json';
 
-request.get(url, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
-  } else {
-    const result = JSON.parse(body);
-  }
+$(document).ready(() => {
+  fetch(`${url}`)
+    .then(response => response.json())
+    .then(data => $('DIV#character').html(data.name))
+    .catch(error => console.error(error));
 });
-
-cName = result.name;
-
-$('div#character').html(cName);
