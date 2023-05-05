3da8fcc1e2f165b72dbb5d44b77c26344ac506f3
MrMvshv <machariajosphat001@gmail.com>
2023-05-05 08:48:55 +0000

    script 4

--- 0x15-javascript-web_jquery/4-script.js
+++ 0x15-javascript-web_jquery/4-script.js
@@ -3,7 +3,7 @@
  * element to red (#FF0000) using jquery
  */
 const head = $('header');
-const red = $('div#red_header');
+const red = $('div#toggle_header');
 
 red.on( "click", function() {
   head.toggleClass("red", "green");
