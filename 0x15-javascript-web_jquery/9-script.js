ea373cc5cecec1601805c896a0cfe611b90bfe96
MrMvshv <machariajosphat001@gmail.com>
2023-05-05 15:13:59 +0000

    script 9

---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,16 @@
+/*
+ *  fetches the character name
+ *  from the url and renders in html
+ */
+'use strict';
+
+const url = 'https://fourtonfish.com/hellosalut/?lang=fr';
+
+$(document).ready(() => {
+  fetch(`${url}`)
+    .then(response => response.json())
+    .then(data => {
+      $('div#hello').text(data.hello);
+    })
+    .catch(error => console.error(error));
+});
